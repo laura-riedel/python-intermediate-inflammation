ef70444512856de3e40bc5ce1a8a8eaab81635b9
Laura Riedel <laura.riedel@uni-potsdam.de>
2024-06-24 12:25:30 +0200

    docs: add input/output info

--- inflammation/models.py
+++ inflammation/models.py
@@ -14,21 +14,29 @@ def load_csv(filename):
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
-    """
+    :returns: a 2D data array"""
     return np.loadtxt(fname=filename, delimiter=',')
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    
+    :param data: a 2D data array
+    :returns: the daily mean"""
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    
+    :param data: a 2D data array
+    :returns: the daily maximum"""
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    :param data: a 2D data array
+    :returns: the daily minimum"""
     return np.min(data, axis=0)
 
