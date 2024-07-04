e14b6eec5823e53cd3a717906611b7fd57580328
Laura Riedel <laura.riedel@uni-potsdam.de>
2024-07-04 10:18:14 +0200

    doc: correct doc of tests for daily_min and daily_max

--- tests/test_models.py
+++ tests/test_models.py
@@ -30,7 +30,7 @@ def test_daily_mean_integers():
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
 def test_daily_min_integers():
-    """Test that mean function works for an array of positive integers."""
+    """Test that min function works for an array of positive integers."""
     from inflammation.models import daily_min
 
     test_input = np.array([[1, 2],
@@ -42,7 +42,7 @@ def test_daily_min_integers():
     npt.assert_array_equal(daily_min(test_input), test_result)
 
 def test_daily_max_integers():
-    """Test that mean function works for an array of positive integers."""
+    """Test that max function works for an array of positive integers."""
     from inflammation.models import daily_max
 
     test_input = np.array([[1, 2],
