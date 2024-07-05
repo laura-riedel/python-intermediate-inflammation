4fba9bcb870eb69bfdc2dd5e2db6507118371d21
Laura Riedel <laura.riedel@uni-potsdam.de>
2024-07-05 13:30:32 +0200

    refactor: make pure function for computing std by day

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -24,21 +24,31 @@ class CSVDataSource():
         if len(data_file_paths) == 0:
             raise ValueError(f"No inflammation data CSV files found in path {self.data_dir}")
         return list(map(models.load_csv, data_file_paths))
+    
+def compute_standard_deviation_by_day(data):
+    """
+    Computes the mean inflammation value for each day across all datasets,
+    then computes the daily standard deviation
+    """
+    means_by_day = map(models.daily_mean, data)
+    means_by_day_matrix = np.stack(list(means_by_day))
+
+    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+    return daily_standard_deviation
 
 def analyse_data(data_source):
     """Calculates the standard deviation by day between datasets.
 
     Gets all the inflammation data from CSV files within a directory,
-    works out the mean inflammation value for each day across all datasets,
+    works out the standard deviation of mean inflammation values 
+    for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
     data = data_source.load_data()
 
-    means_by_day = map(models.daily_mean, data)
-    means_by_day_matrix = np.stack(list(means_by_day))
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
 
-    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
-
-    graph_data = {
-        'standard deviation by day': daily_standard_deviation,
-    }
-    views.visualize(graph_data)
+    # graph_data = {
+    #     'standard deviation by day': daily_standard_deviation,
+    # }
+    # views.visualize(graph_data)
+    return daily_standard_deviation
